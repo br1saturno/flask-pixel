8fc5a57e54c7e08f5a6c6f5ce40a3dff7d807ffc
br1saturno <brunosaturno@gmail.com>
2022-11-04 07:04:45 -0400

    routes studio

--- apps/studio/routes.py
+++ apps/studio/routes.py
@@ -29,6 +29,28 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+@blueprint.route('/<template>')
+@login_required
+def route_template(template):
+
+    try:
+
+        if not template.endswith('.html'):
+            template += '.html'
+
+        # Detect the current page
+        segment = get_segment(request)
+
+        # Serve the file (if exists) from app/templates/home/FILE.html
+        return render_template("home/" + template, segment=segment)
+
+    except TemplateNotFound:
+        return render_template('home/page-404.html'), 404
+
+    except:
+        return render_template('home/page-500.html'), 500
+
+
 @blueprint.route('/studio', methods=['POST'])
 @login_required
 def generate_image():
