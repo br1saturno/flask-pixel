96812fec782f9df03ccf6c8b6ffef25af3119b5d
br1saturno <brunosaturno@gmail.com>
2022-11-13 15:58:27 -0500

    Closing Sunday Nov 13

--- apps/studio/todos.py
+++ apps/studio/todos.py
@@ -1,8 +1,4 @@
-# TODO Image displays big when thumbnail is clicked
-# TODO Info of each image in a popover
-# TODO Add "quality" field to form (steps)
-# TODO Include the variations function to routes/util
-# TODO Images from same session appear at the bottom of the images card (maybe an iframe so it's scrollable)
+# TODO Expand info of session in info popover
 # TODO Redo database with all needed columns
-# TODO Variations UI
 # TODO Vertical menu
+
